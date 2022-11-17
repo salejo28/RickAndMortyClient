ca4eae81d3533bd8a29f37e70f4e723da68240f3
Santiago Ruiz <santiagoruiz@Santiagos-Mac-mini.local>
2022-11-16 21:41:08 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Rick And Morty Client
+
+## Steps
+
+### Download the code
+
+```
+git clone https://github.com/santi280403/RickAndMortyClient.git
+```
+
+### Navigate
+
+```
+cd RickAndMortyClient
+```
+
+### Install dependencies
+
+```
+npm install
+```
+
+o
+
+```
+yarn
+```
+
+### Run the project (dev)
+
+```
+npm run dev
+```
+
+### Run the project (prod)
+
+```
+npm run build
+```
+
+y
+
+```
+npm start
+```
--- src/components/molecules/AppBar/AppBar.tsx
+++ src/components/molecules/AppBar/AppBar.tsx
@@ -1,26 +1,10 @@
-import {
-  Box,
-  IconButton,
-  Toolbar,
-  AppBar as AppBarMUI,
-  Typography,
-} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
+import { Box, Toolbar, AppBar as AppBarMUI, Typography } from '@mui/material'
 
 export const AppBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBarMUI position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Rick and Morty
           </Typography>
